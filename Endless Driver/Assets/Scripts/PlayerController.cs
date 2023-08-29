ce2eb8e9437cfa4111c4496f17fbdc99caa9b844
Ugochukwu Nwankiti <kitisugochukwu@gmail.com>
2023-08-29 09:51:47 +0100

    PlayerMotion Implemented

---
+++ Endless Driver/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float playerMovement = Input.GetAxis("Horizontal");
+        playerRb.AddForce(Vector3.right * speed * playerMovement, ForceMode.Force);
+    }
+}
